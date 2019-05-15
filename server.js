0d51ca8ec07d9a52d33caea1194026e84e1a1e24
Jason Gauvin <jasongauvin@MacBook-Pro-de-Jason.local>
2019-05-15 15:26:04 +0200

    ajout de ejs

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 /*
 Importer les composants serveur
 */
+    // NodeJS
     require('dotenv').config();
     const express = require('express');
     const path = require('path');
+
+    // Inner (notre appli)
+    const frontRouter = require('./routes/front.router');
 //
 
 /*
@@ -13,9 +17,15 @@ Configuration du serveur
     const server = express();
     const port = process.env.PORT;
 
+    // Configuration du moteur de rendu
+    server.set('view engine', 'ejs'); 
+
     // Définition du dossier static du client
     server.set( 'views', __dirname + '/www' );
     server.use( express.static(path.join(__dirname, 'www')) );
+
+    // Utilisation des routers
+    server.use('/', frontRouter);
 //
 
 /* 
