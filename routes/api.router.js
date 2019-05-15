eaeb8c711b3944570476977923d41aca3e61f77c
Jason Gauvin <jasongauvin@MacBook-Pro-de-Jason.local>
2019-05-15 16:43:03 +0200

    Ajout de sql

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,3 +1,4 @@
+
     
 /*
 Configurer le module de route
@@ -6,16 +7,62 @@ const express = require('express');
 const router = express.Router();
 //
 
-/*
-Définition des routes
+/* 
+Configure MySQL
 */
-// Accueil
-router.get('/', (req, res) => {
-    res.json(
-        {
-        msg: 'Hello API'
-    }
-    );
+const mysql = require('mysql');
+const connexion = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    port: 8889,
+    database : 'node-boiler-plate'
+});
+connexion.connect();
+//
+
+/*
+Définition du CRUD
+*/
+// Create Item: POST
+router.post('/article', (req, res) => {
+
+    /* 
+        Pour créer un article il faut une valeur pour :
+        - title
+        - content
+    */
+
+
+    res.json({ msg: 'Create Article' })
+});
+
+// Read all Items: GET
+router.get('/article', (req, res) => {
+    res.json({ msg: 'Read all Article' })
+});
+
+// Read one Item: GET
+router.get('/article/:id', (req, res) => {
+    res.json({ msg: 'Read one Article' })
+});
+
+// Update one Item: PUT
+router.put('/article/:id', (req, res) => {
+
+    /* 
+        Pour éditer un article il faut une valeur pour :
+        - title
+        - content
+    */
+
+
+    res.json({ msg: 'Update one Article' })
+});
+
+// Delete one Item: DELETE
+router.delete('/article/:id', (req, res) => {
+    res.json({ msg: 'Delete one Article' })
 });
 //
 
